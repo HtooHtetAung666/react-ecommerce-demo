8a0e8aaf02a84422d106cf39f9e261e7c12ba847
HtooHtetAung666 <htoohtetaung26@gmail.com>
2023-08-28 15:31:02 +0630

    change logo for website

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useContext } from "react";
 import { AiOutlineShoppingCart } from "react-icons/ai";
-import { DiReact } from "react-icons/di";
+import { SiMsi } from "react-icons/si";
 import { Link } from "react-router-dom";
 import { ShopContext } from "../context/ShopContext";
 
@@ -13,7 +13,7 @@ export const Navbar = () => {
     <nav className=" bg-white border-b-2 border-slate-400 shadow-md sticky top-0">
       <div className="w-full h-full flex items-center justify-between p-2 lg:p-3 lg:max-w-7xl lg:mx-auto">
         <div className="">
-          <DiReact className="w-12 h-12 md:w-16 md:h-16 text-indigo-500 hover:scale-110" />
+          <SiMsi className="w-12 h-12 md:w-14 md:h-14 text-[#7315e5] hover:scale-110" />
         </div>
         <div className="flex items-center space-x-5 mr-5 md:mr-10">
           <Link to="/">
